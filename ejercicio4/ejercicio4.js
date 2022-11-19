c0d747f8a0bbcd9fb2c857642741babf317d615a
Carlos <carcastrojara@gmail.com>
2022-11-18 21:37:01 -0300

    Correciones en ejercicios

--- ejercicio4/ejercicio4.js
+++ ejercicio4/ejercicio4.js
@@ -1,16 +1,16 @@
-function calcularColor(numero) {
-    var cadena;
+function calcularColor(numero) { //se agrega el argumento 
+    var color;
     if (numero === 1){
-        cadena='El color es negro';
-    }else if (numero === 2){
-        cadena='El color es blanco';
+        color='negro';//se cambia black a negro
+    }else if (numero === 2){//Estaba como asignacion, se cambia a comparacion, tambien se modifica a else if
+        color='blanco';
     }else if (numero === 3){
-        cadena='El color es azul';
+        color='azul';
     }else{
-        cadena='El color es verde';}
-    return cadena;
+        color='verde';}
+    return "El color es " + color;
 }
 console.log(calcularColor(1));
 console.log(calcularColor(2));
 console.log(calcularColor(3));
-console.log(calcularColor(4));
+console.log(calcularColor(8));
