fbfcdf823bdbc9bc4fb370bd68e9e83e144d65b3
Carlos <carcastrojara@gmail.com>
2022-11-23 21:15:58 -0300

    Ejercicio 6 terminado

---
+++ ejercicio6/index.js
@@ -0,0 +1,23 @@
+//Creacion de la variable pedro de tipo objeto
+const pedro = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    activo: true,
+    hobbies: ['programar', 'squash', 'piano']
+}
+//Imprime en la consola el valor de la llave edad.
+console.log(pedro.edad)
+//Agrega una propiedad con llave estatura y valor 1.8
+pedro.estatura = 1.8
+//Elimina la propiedad con llave activo
+delete pedro.activo
+//Recorre todas las propiedades e imprímelas en consola 
+for (var llave in pedro) {
+    if (pedro.hasOwnProperty(llave)) {
+      console.log(llave + ': '+pedro[llave]+ "\n")
+    }
+  }
+//Agrégale al objeto una función llamada saluda 
+pedro.saluda= function() { return "Hola, me llamo " + pedro.nombre} 
+//Llama la función saluda y verifica el resultado
+console.log(pedro.saluda())
